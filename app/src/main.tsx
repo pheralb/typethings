8e0c9be305278d3777e04b30e2cce862f353ea6f
pheralb <pablohdez4340@gmail.com>
2023-08-19 00:06:45 +0100

    Add react-router-dom config.

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Styles:
 import "@/styles/globals.css";
 
-// App:
-import App from "./App";
+// Global layout:
+import Sidebar from "./components/sidebar";
+
+// Routes:
+import App from "./routes";
+
+// Router:
+const router = createBrowserRouter([
+  {
+    element: <Sidebar />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
