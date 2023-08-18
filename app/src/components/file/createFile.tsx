a320fac6a7dbc8b7f572a054d0bcde93bd33beb3
pheralb <pablohdez4340@gmail.com>
2023-08-19 00:13:54 +0100

    Initial createFile dialog.

--- app/src/components/file/createFile.tsx
+++ app/src/components/file/createFile.tsx
@@ -1,5 +1,67 @@
-const CreateFile = () => {
-  return <div className="flex flex-col space-y-4">CreateFile</div>;
+import type { ReactNode } from "react";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { createNewFile } from "@/functions/createNewFile";
+
+interface iCreateFileProps {
+  trigger: ReactNode;
+}
+
+interface iCreateFileInputs {
+  title: string;
+}
+
+const CreateFile = (props: iCreateFileProps) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<iCreateFileInputs>();
+  const [open, setOpen] = useState<boolean>(false);
+
+  // Create New File function:
+  const handleCreateFile: SubmitHandler<iCreateFileInputs> = async (data) => {
+    try {
+      await createNewFile({
+        folder: "taurifiles",
+        filename: data.title,
+        extension: "md",
+        content: "hello world",
+      });
+      setOpen(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{props.trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>New file</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit(handleCreateFile)}>
+          <Input
+            id="title"
+            className="mt-1"
+            placeholder="Enter title..."
+            {...register("title", { required: true })}
+          />
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CreateFile;
