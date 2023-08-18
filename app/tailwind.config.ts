68c53401fde75f2209046412b779cd26349f829d
pheralb <pablohdez4340@gmail.com>
2023-08-19 00:06:24 +0100

    Add custom font.

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -18,6 +18,9 @@ const config: Config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["Inter-Roman", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
