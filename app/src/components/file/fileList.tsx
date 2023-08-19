98eb5698ce728410c4a73aba20208609bd1c1b5d
pheralb <pablohdez4340@gmail.com>
2023-08-19 17:04:03 +0100

    Show files from folder.

--- app/src/components/file/fileList.tsx
+++ app/src/components/file/fileList.tsx
@@ -1,4 +1,21 @@
+import { desktopDir } from "@tauri-apps/api/path";
+import { useEffect } from "react";
+import { readFiles } from "@/functions/readFiles";
+
 const FileList = () => {
+  // Read files from directory:
+  useEffect(() => {
+    async function loadFiles() {
+      const desktopPath = await desktopDir();
+      const result = await readFiles({
+        directory: desktopPath,
+        folder: "taurifiles",
+      });
+      console.log(result);
+    }
+    loadFiles();
+  }, []);
+
   return <div>FileList</div>;
 };
 
