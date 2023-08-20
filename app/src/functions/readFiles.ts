94923858896ddec9dba3c260da68538a298aa759
pheralb <pablohdez4340@gmail.com>
2023-08-20 13:12:29 +0100

    Update readFilesFromFolder & add readFile + add join from tauri api.

--- app/src/functions/readFiles.ts
+++ app/src/functions/readFiles.ts
@@ -1,10 +1,32 @@
-import { readDir } from "@tauri-apps/api/fs";
+import { readDir, readTextFile } from "@tauri-apps/api/fs";
+import { join } from "@tauri-apps/api/path";
 
-interface iReadFiles {
+interface iReadFilesFromFolder {
   directory: string;
   folder: string;
 }
 
-export const readFilesFromFolder = async ({ directory, folder }: iReadFiles) => {
-  return await readDir(`${directory}/${folder}`);
+interface iReadFile {
+  directory: string;
+  folder: string;
+  filename: string;
+  extension: string;
+}
+
+export const readFilesFromFolder = async ({
+  directory,
+  folder,
+}: iReadFilesFromFolder) => {
+  const fullPath = await join(directory, folder);
+  return await readDir(fullPath);
+};
+
+export const readFile = async ({
+  directory,
+  folder,
+  filename,
+  extension,
+}: iReadFile) => {
+  const fullPath = await join(directory, folder, `${filename}.${extension}`);
+  return await readTextFile(fullPath);
 };
