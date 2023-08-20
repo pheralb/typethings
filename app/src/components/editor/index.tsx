ac59a280b7eb14a0798a1e929d944e98cbcf12d1
pheralb <pablohdez4340@gmail.com>
2023-08-20 14:15:28 +0100

    Update index page + editor.

--- app/src/components/editor/index.tsx
+++ app/src/components/editor/index.tsx
@@ -1,7 +1,8 @@
+import type { EditorProps } from "@monaco-editor/react";
 import { Editor } from "@monaco-editor/react";
 
-const CustomEditor = () => {
-  return <Editor theme="vs-dark" height="100vh" />;
+const CustomEditor = (props: EditorProps) => {
+  return <Editor theme="vs-dark" height="100vh" {...props} />;
 };
 
 export default CustomEditor;
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
+import { desktopDir } from "@tauri-apps/api/path";
+
+import { useFilesStore } from "@/store/filesStore";
 import Editor from "../components/editor";
+import { createUpdateFile } from "@/functions/createUpdateFile";
 
 function App() {
+  const fileSelected = useFilesStore((state) => state.selectedFile);
+  const [text, setText] = useState<string | undefined>("");
+  const [isSaved, setIsSaved] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!fileSelected) return;
+
+    const saveFile = setTimeout(async () => {
+      try {
+        const desktopPath = await desktopDir();
+        await createUpdateFile({
+          directory: desktopPath,
+          folder: "taurifiles",
+          filename: fileSelected.filename,
+          extension: fileSelected.extension,
+          content: text ?? "",
+        });
+      } catch (error) {
+        console.error(error);
+      }
+      setIsSaved(true);
+    }, 1000);
+
+    return () => clearTimeout(saveFile);
+  }, [text]);
+
   return (
     <>
-      <Editor />
+      {fileSelected ? (
+        <Editor
+          language={fileSelected.extension}
+          onChange={(value) => setText(value)}
+          value={fileSelected.content}
+        />
+      ) : (
+        <div className="flex h-screen items-center justify-center">
+          No file selected
+        </div>
+      )}
     </>
   );
 }
