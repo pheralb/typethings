f7379cdccae56d04bb4c3d74ec7599b89a237af5
pheralb <pablohdez4340@gmail.com>
2023-08-28 17:15:27 +0100

    Update file store with removeFile function.

--- app/src/store/filesStore.ts
+++ app/src/store/filesStore.ts
@@ -9,9 +9,10 @@ type iSelectedFile = {
 interface iFilesStore {
   files: string[];
   selectedFile: iSelectedFile | null;
-  setSelectedFile: ({ filename, extension, content }: iSelectedFile) => void;
+  setSelectedFile: ({ filename, extension, content }: iSelectedFile) => void | null;
   saveFilesToStore: (files: string[]) => void;
   addFile: (file: string) => void;
+  removeFile: (filename: string) => void;
 }
 
 export const useFilesStore = create<iFilesStore>((set) => ({
@@ -27,4 +28,6 @@ export const useFilesStore = create<iFilesStore>((set) => ({
     }),
   saveFilesToStore: (files) => set({ files }),
   addFile: (file) => set((state) => ({ files: [...state.files, file] })),
+  removeFile: (filename) =>
+    set((state) => ({ files: state.files.filter((f) => f !== filename) })),
 }));
