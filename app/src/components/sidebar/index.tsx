aad2ccc249bbaec15520081cc1512043a9839e95
pheralb <pablohdez4340@gmail.com>
2023-09-30 20:39:08 +0100

    Update sidebar with resizable option.

--- app/src/components/sidebar/index.tsx
+++ app/src/components/sidebar/index.tsx
@@ -11,102 +11,135 @@ import FileList from "@/components/file/fileList";
 import SidebarGroup from "@/components/sidebar/sidebarGroup";
 import Folder from "@/components/folder";
 import ManageWorkspaces from "@/components/workspaces/manageWorkspaces";
+import { useEffect, useRef, useState } from "react";
 
 // Global styles:
 export const SidebarItemClasses = cn("w-full justify-start text-sm px-2");
 export const SidebarItemIconSize = 16;
 
-const SidebarContent = () => {
-  const workspaces = useWorkspaceStore((state) => state.workspaces);
-  return (
-    <nav
-      className={cn(
-        "fixed left-0 top-0 h-full",
-        "w-52 pb-10",
-        "overflow-y-auto overflow-x-hidden",
-        "bg-neutral-800/20",
-        "border-r border-neutral-800",
-      )}
-    >
-      <div className="w-full items-center px-4 py-5">
-        <SidebarGroup border={true}>
-          <CreateFile
-            trigger={
-              <Button variant="ghost" className={SidebarItemClasses}>
-                <div className="flex items-center space-x-3">
-                  <Plus size={SidebarItemIconSize} />
-                  <span>New file</span>
-                </div>
-              </Button>
-            }
-          />
-          <OpenFile
-            trigger={
-              <Button variant="ghost" className={SidebarItemClasses}>
-                <div className="flex items-center space-x-3">
-                  <FilePlus2Icon size={SidebarItemIconSize} />
-                  <span>Open file</span>
-                </div>
-              </Button>
-            }
-          />
-          <ManageWorkspaces
-            trigger={
-              <Button variant="ghost" className={SidebarItemClasses}>
-                <div className="flex items-center space-x-3">
-                  <Folders size={SidebarItemIconSize} />
-                  <span>Workspaces</span>
-                </div>
-              </Button>
-            }
-          />
-          <Link
-            to="/test"
-            className={cn(
-              buttonVariants({ variant: "ghost" }),
-              SidebarItemClasses,
-            )}
-          >
-            <div className="flex items-center space-x-3">
-              <Search size={SidebarItemIconSize} />
-              <span>Search</span>
-            </div>
-          </Link>
-          <Link
-            to="/settings"
-            className={cn(
-              buttonVariants({ variant: "ghost" }),
-              SidebarItemClasses,
-            )}
-          >
-            <div className="flex items-center space-x-3">
-              <Settings size={SidebarItemIconSize} />
-              <span>Settings</span>
-            </div>
-          </Link>
-        </SidebarGroup>
-        <SidebarGroup title="Workspaces">
-          <div className="flex flex-col space-y-1">
-            {workspaces.map((workspace) => (
-              <Folder key={workspace.folderPath} name={workspace.folderName}>
-                <FileList
-                  directory={workspace.folderPath}
-                  folder={workspace.folderName}
-                />
-              </Folder>
-            ))}
-          </div>
-        </SidebarGroup>
-      </div>
-    </nav>
-  );
-};
+// Sidebar Config:
+const [minWidth, maxWidth, defaultWidth] = [200, 300, 208];
 
 const Sidebar = () => {
+  const workspaces = useWorkspaceStore((state) => state.workspaces);
+  const [width, setWidth] = useState<number>(defaultWidth);
+  const isResized = useRef(false);
+
+  // Resize sidebar:
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      if (!isResized.current) {
+        return;
+      }
+      setWidth((previousWidth) => {
+        const newWidth = previousWidth + e.movementX / 2;
+        const isWidthInRange = newWidth >= minWidth && newWidth <= maxWidth;
+        return isWidthInRange ? newWidth : previousWidth;
+      });
+    });
+
+    window.addEventListener("mouseup", () => {
+      isResized.current = false;
+    });
+  }, []);
+
   return (
     <main className="min-h-screen">
-      <SidebarContent />
-      <div className="ml-52">
+      <nav
+        className={cn(
+          "fixed left-0 top-0 h-full pb-10",
+          "overflow-y-auto overflow-x-hidden",
+          "bg-neutral-800/20",
+          "border-r border-neutral-800",
+        )}
+        style={{ width: `${width / 16}rem` }}
+      >
+        <div className="w-full items-center px-4 py-5">
+          <SidebarGroup border={true}>
+            <CreateFile
+              trigger={
+                <Button variant="ghost" className={SidebarItemClasses}>
+                  <div className="flex items-center space-x-3">
+                    <Plus size={SidebarItemIconSize} />
+                    <span>New file</span>
+                  </div>
+                </Button>
+              }
+            />
+            <OpenFile
+              trigger={
+                <Button variant="ghost" className={SidebarItemClasses}>
+                  <div className="flex items-center space-x-3">
+                    <FilePlus2Icon size={SidebarItemIconSize} />
+                    <span>Open file</span>
+                  </div>
+                </Button>
+              }
+            />
+            <ManageWorkspaces
+              trigger={
+                <Button variant="ghost" className={SidebarItemClasses}>
+                  <div className="flex items-center space-x-3">
+                    <Folders size={SidebarItemIconSize} />
+                    <span>Workspaces</span>
+                  </div>
+                </Button>
+              }
+            />
+            <Link
+              to="/test"
+              className={cn(
+                buttonVariants({ variant: "ghost" }),
+                SidebarItemClasses,
+              )}
+            >
+              <div className="flex items-center space-x-3">
+                <Search size={SidebarItemIconSize} />
+                <span>Search</span>
+              </div>
+            </Link>
+            <Link
+              to="/settings"
+              className={cn(
+                buttonVariants({ variant: "ghost" }),
+                SidebarItemClasses,
+              )}
+            >
+              <div className="flex items-center space-x-3">
+                <Settings size={SidebarItemIconSize} />
+                <span>Settings</span>
+              </div>
+            </Link>
+          </SidebarGroup>
+          <SidebarGroup title="Workspaces">
+            <div className="flex flex-col space-y-1">
+              {workspaces.map((workspace) => (
+                <Folder
+                  key={workspace.folderPath}
+                  name={workspace.folderName}
+                  path={workspace.folderPath}
+                >
+                  <FileList
+                    directory={workspace.folderPath}
+                    folder={workspace.folderName}
+                  />
+                </Folder>
+              ))}
+            </div>
+          </SidebarGroup>
+        </div>
+        <div
+          className={cn(
+            "absolute bottom-0 right-0 top-0",
+            "w-1 hover:bg-neutral-800/50",
+            "cursor-ew-resize",
+          )}
+          onMouseDown={() => {
+            isResized.current = true;
+          }}
+        />
+      </nav>
+      <div style={{ marginLeft: `${width / 16}rem` }}>
         <Outlet />
       </div>
     </main>
