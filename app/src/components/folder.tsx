49dce8ffe4bd1c6996804b5a564b0b20bf3aca00
pheralb <pablohdez4340@gmail.com>
2023-09-30 20:38:34 +0100

    Add collapsible menu to folder.

--- app/src/components/folder.tsx
+++ app/src/components/folder.tsx
@@ -1,5 +1,11 @@
 import { useState, type ReactNode } from "react";
-import { FolderIcon, FolderOpenIcon } from "lucide-react";
+import {
+  FolderClosed,
+  FolderIcon,
+  FolderOpen,
+  FolderOpenIcon,
+  X,
+} from "lucide-react";
 import { cn } from "@/utils";
 
 import { SidebarItemClasses, SidebarItemIconSize } from "./sidebar";
@@ -9,46 +15,84 @@ import {
   CollapsibleTrigger,
 } from "./ui/collapsible";
 import { Button } from "./ui/button";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "./ui/context-menu";
+import { useWorkspaceStore } from "@/store/workspaceStore";
 
 interface FolderProps {
   name: string;
+  path: string;
   children: ReactNode;
 }
 
 const Folder = (props: FolderProps) => {
   const [openCollapsible, setOpenCollapsible] = useState<boolean>(false);
+  const deleteWorkspace = useWorkspaceStore((state) => state.deleteWorkspace);
+  const selectWorkspace = useWorkspaceStore((state) => state.selectWorkspace);
+
+  const handleDeleteWorkspace = (path: string) => {
+    selectWorkspace(null);
+    deleteWorkspace(path);
+  };
+
   return (
-    <Collapsible
-      id={props.name}
-      open={openCollapsible}
-      onOpenChange={() => setOpenCollapsible(!openCollapsible)}
-    >
-      <CollapsibleTrigger className="w-full" asChild>
-        <Button
-          variant="ghost"
-          className={cn(
-            SidebarItemClasses,
-            "flex cursor-default items-center space-x-3 text-sm text-neutral-400 duration-75",
-            openCollapsible && "text-neutral-100",
-          )}
+    <ContextMenu>
+      <ContextMenuTrigger asChild>
+        <Collapsible
+          id={props.name}
+          open={openCollapsible}
+          onOpenChange={() => setOpenCollapsible(!openCollapsible)}
         >
-          {openCollapsible ? (
-            <FolderOpenIcon size={SidebarItemIconSize} />
-          ) : (
-            <FolderIcon size={SidebarItemIconSize} />
-          )}
-          <span>{props.name}</span>
-        </Button>
-      </CollapsibleTrigger>
-      <CollapsibleContent
-        className={cn(
-          "ml-3 border-l border-neutral-700/50 pl-3 pt-1 transition-all duration-100",
-          openCollapsible ? "animate-in fade-in" : "",
-        )}
-      >
-        {props.children}
-      </CollapsibleContent>
-    </Collapsible>
+          <CollapsibleTrigger className="w-full" asChild>
+            <Button
+              variant="ghost"
+              className={cn(
+                SidebarItemClasses,
+                "my-0 flex cursor-default items-center space-x-3 text-sm text-neutral-400 duration-75",
+                openCollapsible && "text-neutral-100",
+              )}
+            >
+              {openCollapsible ? (
+                <FolderOpenIcon size={SidebarItemIconSize} />
+              ) : (
+                <FolderIcon size={SidebarItemIconSize} />
+              )}
+              <span>{props.name}</span>
+            </Button>
+          </CollapsibleTrigger>
+          <CollapsibleContent
+            className={cn(
+              "ml-3 border-l border-neutral-700/50 pl-3 pt-1 transition-all duration-100",
+              openCollapsible ? "animate-in fade-in" : "",
+            )}
+          >
+            {props.children}
+          </CollapsibleContent>
+        </Collapsible>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem onClick={() => setOpenCollapsible(!openCollapsible)}>
+          <div className="flex items-center space-x-2">
+            {openCollapsible ? (
+              <FolderClosed size={SidebarItemIconSize} />
+            ) : (
+              <FolderOpen size={SidebarItemIconSize} />
+            )}
+            <span>{openCollapsible ? "Hide" : "Show"} files</span>
+          </div>
+        </ContextMenuItem>
+        <ContextMenuItem onClick={() => handleDeleteWorkspace(props.path)}>
+          <div className="flex items-center space-x-2">
+            <X size={SidebarItemIconSize} />
+            <span>Close workspace</span>
+          </div>
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 };
 
