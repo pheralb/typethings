6fc7b9b7d9a07c50e4d47c1f655ab21a44cc3155
pheralb <pablohdez4340@gmail.com>
2023-10-15 14:46:23 +0100

    Add redirect & headers.

--- website/next.config.js
+++ website/next.config.js
@@ -2,7 +2,37 @@
 const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
-  transpilePackages: ['@typethings/ui'],
+  transpilePackages: ["@typethings/ui"],
+  redirect() {
+    return [
+      {
+        source: "/git",
+        destination: "https://github.com/pheralb/typethings",
+        permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer-when-downgrade",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
