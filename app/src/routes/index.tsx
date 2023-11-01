822d9456d4fbb813faf49cf175d97847b3abd732
pheralb <pablohdez4340@gmail.com>
2023-11-01 19:49:18 +0000

    Fix homepage.

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -2,18 +2,6 @@ import Container from "@/components/container";
 import PageNavbar from "@/components/pageNavbar";
 
 function App() {
-  const greeting = () => {
-    const date = new Date();
-    const hour = date.getHours();
-    if (hour < 12) {
-      return "Good morning";
-    } else if (hour < 18) {
-      return "Good afternoon";
-    } else {
-      return "Good evening";
-    }
-  };
-
   return (
     <PageNavbar title="Inbox" border={false}>
       <Container className="pt-0">
