4dc51ca25ae23f3c1a7dec00ddc2a4bc5827186b
pheralb <pablohdez4340@gmail.com>
2023-11-02 12:22:15 +0000

    Update prose classes.

--- packages/ui/src/extend/prose.ts
+++ packages/ui/src/extend/prose.ts
@@ -2,5 +2,5 @@ import { cn } from "..";
 
 export const ProseClasses = cn(
   // Global Prose:
-  "prose prose-quoteless prose-neutral dark:prose-invert prose-p:my-4 prose-pre-code:false prose-a:underline-offset-4 prose-a:decoration-neutral-400 prose-a:decoration-solid prose-a:cursor-pointer prose-a:cursor-ne-resize"
+  "prose prose-quoteless prose-neutral dark:prose-invert prose-p:my-4 prose-a:underline-offset-4 prose-a:decoration-neutral-400 prose-a:decoration-solid prose-a:cursor-pointer prose-a:cursor-ne-resize"
 );
