1e3fd652a3ed624e009af11c0e2d54f1b7cf414d
pheralb <pablohdez4340@gmail.com>
2023-11-04 00:27:38 +0000

    Dialog & radio-group improvements.

--- packages/ui/src/components/dialog.tsx
+++ packages/ui/src/components/dialog.tsx
@@ -72,7 +72,7 @@ const DialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      "flex flex-col-reverse sm:flex-row sm:justify-end space-x-1",
       className,
     )}
     {...props}
--- packages/ui/src/components/radio-group.tsx
+++ packages/ui/src/components/radio-group.tsx
@@ -27,13 +27,13 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border border-neutral-700 shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+        "focus-visible:ring-ring h-4 w-4 rounded-xl border border-neutral-700 shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex cursor-default items-center justify-center">
-        <CheckIcon className="h-3.5 w-3.5" />
+        <CheckIcon className="h-3 w-3" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
